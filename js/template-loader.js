8e90af67cc4998d8a6ae6f6d5375cfa3703ba1ad
OsnaFonz <andreas@liers.org>
2025-08-20 08:34:44 +0200

    Implement code changes to enhance functionality and improve performance

---
+++ js/template-loader.js
@@ -0,0 +1,11 @@
+$(function() {
+	$("#templates").load("../templates.html", function (response, status, xhr) {
+    if (status === "error") {
+      console.error("Error loading templates.html:", xhr.status, xhr.statusText);
+    } else {
+        console.log("templates.html loaded successfully.");
+        $(document).trigger("templateLoaderReady");
+    }
+  });
+
+});
